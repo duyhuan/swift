0d3f5a396c8f415799f66ffd4231ffbbba8c82fc
duyhuan <ndhuan89@yahoo.com.vn>
2016-09-22 13:00:39 +0700

    add edit text

--- EditImage/EditImage/ViewController.swift
+++ EditImage/EditImage/ViewController.swift
@@ -11,29 +11,42 @@ import Social
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource, UITextViewDelegate {
     
+    var arrImage:[ItemModel] = []
+    
+    
     @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    @IBOutlet var pickerView: UIPickerView!
+    
+    @IBOutlet var myView: UIView!
+    
+    @IBOutlet var btnChooseImage: UIButton!
+    @IBOutlet var btnSave: UIButton!
+    @IBOutlet var btnShare: UIButton!
+    
     var txtv:UITextView = UITextView()
+    var arrTxtv:[UITextView] = []
+    
     let panGesture:UIPanGestureRecognizer = UIPanGestureRecognizer()
     let tapGesture:UITapGestureRecognizer = UITapGestureRecognizer()
     let tapGesture1:UITapGestureRecognizer = UITapGestureRecognizer()
-    @IBOutlet var collectionView: UICollectionView!
-    
+
     let imagePicker = UIImagePickerController()
-    @IBOutlet var pickerView: UIPickerView!
     
-    let arrItem:[[String]] = [UIFont.familyNames(), ["blue", "red", "gray", "green", "black"]]
     let color:[UIColor] = [UIColor.blueColor(), UIColor.redColor(), UIColor.grayColor(), UIColor.greenColor(), UIColor.blackColor()]
+    let arrItem:[[String]] = [UIFont.familyNames(), ["blue", "red", "gray", "green", "black"]]
     
-    var arrImage:[ItemModel] = []
-    @IBOutlet var myView: UIView!
-    
-    @IBAction func btn(sender: AnyObject) {
+    // MARK: Button Share
+    @IBAction func btnShare(sender: AnyObject) {
         let composeSheet = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
         composeSheet.addImage(imageView.image)
         
         presentViewController(composeSheet, animated: true, completion: nil)
     }
     
+    // MARK: Button Choose Image
     @IBAction func loadImageButtonTapped(sender: UIButton) {
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .PhotoLibrary
@@ -41,12 +54,51 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    // MARK: Aligment TextView
+    
+    @IBOutlet var aligment: UISegmentedControl!
+    
+    @IBAction func alignment(sender: UISegmentedControl) {
+        switch aligment.selectedSegmentIndex {
+        case 0:
+            for i in 0..<arrTxtv.count {
+                if arrTxtv[i].tag == 1 {
+                    arrTxtv[i].textAlignment = NSTextAlignment.Left
+                }
+            }
+        case 1:
+            for i in 0..<arrTxtv.count {
+                if arrTxtv[i].tag == 1 {
+                    arrTxtv[i].textAlignment = NSTextAlignment.Center
+                }
+            }
+        case 2:
+            for i in 0..<arrTxtv.count {
+                if arrTxtv[i].tag == 1 {
+                    arrTxtv[i].textAlignment = NSTextAlignment.Right
+                }
+            }
+        default:
+            print("a")
+        }
+    }
+    
+    // MARK: Set TextView Font Size
+    @IBOutlet var lbl: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBAction func change(sender: UISlider) {
+        lbl.text = String(Int(slider.value))
+        for i in 0..<arrTxtv.count {
+            if arrTxtv[i].tag == 1  {
+                arrTxtv[i].font = arrTxtv[i].font!.fontWithSize(CGFloat(slider.value))
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        
-        txtv.delegate = self
         
         tapGesture.addTarget(self, action: #selector(ViewController.tapGesture(_:)))
         tapGesture.numberOfTapsRequired = 2
@@ -54,6 +106,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         tapGesture1.addTarget(self, action: #selector(ViewController.tapGesture1(_:)))
         imageView.addGestureRecognizer(tapGesture1)
+        
+        panGesture.addTarget(self, action: #selector(ViewController.panGesture(_:)))
         
         pickerView.delegate = self
         pickerView.dataSource = self
@@ -67,6 +121,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         arrImage.append(ItemModel.init(img: "4.jpg"))
         arrImage.append(ItemModel.init(img: "5.jpg"))
 
+        btnChooseImage.layer.cornerRadius = 5
+        btnSave.layer.cornerRadius = 5
+        btnShare.layer.cornerRadius = 5
+        
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.value = 1
+        lbl.text = "1"
     }
     
     // MARK: - UIImagePickerControllerDelegate Methods
@@ -84,6 +146,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    // MARK: Button Save
     @IBAction func btnSaveClicked(sender: AnyObject) {
         txtv.endEditing(false)
         UIGraphicsBeginImageContext(imageView.frame.size)
@@ -91,31 +154,57 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        //imageView.image = image
-        txtv.removeFromSuperview()
+        imageView.image = image
+        for i in 0..<arrTxtv.count {
+            arrTxtv[i].removeFromSuperview()
+        }
     }
     
     // MARK: Tap Create txtv
     func tapGesture(sender: UITapGestureRecognizer) {
-        txtv = UITextView()
-        txtv.userInteractionEnabled = true
-        txtv.multipleTouchEnabled = true
-        txtv.delegate = self
-        txtv.becomeFirstResponder()
         let point:CGPoint = sender.locationInView(imageView)
-        txtv.frame =  CGRect(x: point.x, y: point.y, width: 100, height: 30)
+        txtv = UITextView()
         txtv.backgroundColor = UIColor.redColor()
-        panGesture.addTarget(self, action: #selector(ViewController.panGesture(_:)))
-        txtv.addGestureRecognizer(panGesture)
+        txtv.userInteractionEnabled = true
+        txtv.delegate = self
+        txtv.frame =  CGRect(x: point.x, y: point.y, width: 100, height: 30)
+        txtv.becomeFirstResponder()
+        arrTxtv.append(txtv)
         imageView.addSubview(txtv)
     }
     
+    // MARK: PanGesture
     func panGesture(sender: UIPanGestureRecognizer) {
-        let translation = sender.translationInView(self.view)
-        sender.view?.center = CGPoint(x: translation.x + (sender.view?.center.x)!, y: translation.y + (sender.view?.center.y)!)
-        sender.setTranslation(CGPointZero, inView: view)
+        
+        let location = sender.locationInView(imageView)
+        let someRect = imageView.bounds
+        if (CGRectContainsPoint(someRect, location)) {
+            let translation = sender.translationInView(self.imageView)
+            sender.view?.center = CGPoint(x: translation.x + (sender.view?.center.x)!, y: translation.y + (sender.view?.center.y)!)
+            sender.setTranslation(CGPointZero, inView: imageView)
+        }
     }
     
+    func textViewDidBeginEditing(textView: UITextView) {
+        for i in 0..<arrTxtv.count {
+            arrTxtv[i].tag = 0
+            if textView == arrTxtv[i] {
+                arrTxtv[i].tag = 1
+                arrTxtv[i].addGestureRecognizer(panGesture)
+            }
+        }
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        for i in (0..<arrTxtv.count).reverse(){
+            if textView == arrTxtv[i] && textView.text == "" {
+                arrTxtv[i].removeFromSuperview()
+                arrTxtv.removeAtIndex(i)
+            }
+        }
+    }
+    
+    // MARK: PickerView
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 2
     }
@@ -125,13 +214,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
-        
         if component == 1 {
-        
             let textColor:UIView = UIView(frame: CGRectMake(0, 0, 50, 50))
             textColor.backgroundColor = color[row]
             return textColor
-
         } else {
             let textFont:UILabel = UILabel(frame: CGRectMake(0, 0, 200 , 50))
             textFont.text = arrItem[0][row]
@@ -142,12 +228,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if component == 1 {
-            txtv.textColor = color[row]
+            for i in 0..<arrTxtv.count {
+                if arrTxtv[i].tag == 1 {
+                    arrTxtv[i].textColor = color[row]
+                }
+            }
         } else {
-            txtv.font = UIFont(name: arrItem[0][row], size: 14)
+            for i in 0..<arrTxtv.count {
+                if arrTxtv[i].tag == 1 {
+                    arrTxtv[i].font = UIFont(name: arrItem[0][row], size: 14)
+                }
+            }
         }
     }
     
+    // MARK: CollectionView
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return arrImage.count
     }
@@ -172,14 +267,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.imageView.backgroundColor = UIColor(patternImage: imageView)
     }
     
-    func textViewDidEndEditing(textView: UITextView) {
-        if textView.text == "" {
-            textView.removeFromSuperview()
-        }
-    }
-    
+    // MARK: Func tapGesture1
     func tapGesture1(sender: UITapGestureRecognizer) {
-        txtv.endEditing(false)
+        for i in 0..<arrTxtv.count {
+            arrTxtv[i].endEditing(false)
+        }
     }
     
 }
